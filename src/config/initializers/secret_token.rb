dd8638b647da53e614392c3477de4b933d709eed
mstMINAMI <mst.minami@gmail.com>
2013-02-09 17:15:13 +0900

    update

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asakatsu::Application.config.secret_token = '66748fc9f8f6fe51794eaf168bdf36fca80a0d588886a56f4e369ee34611cb16e7f255de3ec64978e831acc171a0a9686a94b2b372031dbd6da58787bd76317f'
